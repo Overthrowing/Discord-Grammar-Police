a751c87594d55308686a6b4d214c61638f1ae4d8
Siddharth R <Siddharth.Radh@gmail.com>
2021-06-05 11:10:03 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import discord
+from discord.ext import commands
+import language_tool_python
+from dotenv import load_dotenv
+
+load_dotenv()
+tool = language_tool_python.LanguageTool('en-US')
+
+
+class Grammar_Police(commands.Bot):
+    async def on_ready(self):
+        print(f'{self.user.name} has connected to Discord!')
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        message_content = message.content
+        user_id = message.author.id
+
+        errors = tool.check(message_content)
+        if len(errors) > 0:
+            corrected = tool.correct(message_content)
+            await message.channel.send("*" + corrected + f" <@!{user_id}>")
+
+        await self.process_commands(message)
+
+
+bot = Grammar_Police(
+    command_prefix=commands.when_mentioned_or(','),
+    help_command=None,
+    intents=discord.Intents.all()
+)
+
+bot.run(os.getenv("DISCORD_TOKEN"))
